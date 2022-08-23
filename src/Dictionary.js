cbd1a15bd1165e9a9f3c6b3fd70330338727175f
tvuleta8 <110557486+tvuleta8@users.noreply.github.com>
2022-08-23 18:42:45 +0200

    rendered word meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import axios from "axios";
 import "./Dictionary.css";
+import Results from "./Results";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
-    console.log(response.data);
+    setResults(response.data[0]);
   }
 
   function search(event) {
@@ -33,6 +35,7 @@ export default function Dictionary() {
           <input type="submit" value="Search" className="col-2 search-button" />
         </form>
       </div>
+      <Results results={results} />
     </div>
   );
 }

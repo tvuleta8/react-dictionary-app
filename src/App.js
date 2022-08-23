d4f3a147ca39616d0157e458ad7764bfa6f12fea
tvuleta8 <110557486+tvuleta8@users.noreply.github.com>
2022-08-23 20:23:56 +0200

    added synonyms component

--- src/App.js
+++ src/App.js
@@ -6,16 +6,16 @@ function App() {
     <div className="App">
       <div className="container">
         <Dictionary />
+        <footer>
+          <a
+            href="https://github.com/tvuleta8/react-dictionary-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Source Code
+          </a>
+        </footer>
       </div>
-      <footer>
-        <a
-          href="https://github.com/tvuleta8/react-dictionary-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Source Code
-        </a>
-      </footer>
     </div>
   );
 }
--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
   return (
@@ -7,11 +8,12 @@ export default function Meaning(props) {
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <p>
-              {definition.definition}
-              <br />
-              <em>{definition.example}</em>
-            </p>
+            <strong>Definition: </strong>
+            {definition.definition}
+            <br />
+            <strong>Example: </strong>
+            <em>{definition.example}</em>
+            <Synonyms synonyms={definition.synonyms} />
             <hr />
           </div>
         );
